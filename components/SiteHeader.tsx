0e02fab66f8adb3ca501f2eb8149f14149437450
MissionTS <westin@missionts.com>
2026-09-21 09:26:52 -0400

    Use white wordmark throughout navy mobile and tablet header

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -49,8 +49,8 @@ export function SiteHeader() {
       </div>
       <div className="relative mx-auto flex max-w-7xl items-center justify-between bg-mission-navy px-6 py-4 lg:bg-transparent lg:px-8">
         <Link href="/" aria-label="Mission Technology Solutions home" className="shrink-0" onClick={() => setMenuOpen(false)}>
-          <Image src="/brand/mission-wordmark-white.png" alt="Mission Technology Solutions" width={4980} height={539} priority className="absolute left-1/2 top-1/2 h-auto w-[255px] -translate-x-1/2 -translate-y-1/2 sm:hidden" />
-          <Image src="/brand/mission-wordmark-blue.png" alt="Mission Technology Solutions" width={4980} height={539} priority className="hidden h-auto w-[260px] sm:block xl:w-[300px]" />
+          <Image src="/brand/mission-wordmark-white.png" alt="Mission Technology Solutions" width={4980} height={539} priority className="absolute left-1/2 top-1/2 h-auto w-[255px] -translate-x-1/2 -translate-y-1/2 sm:static sm:w-[260px] sm:translate-x-0 sm:translate-y-0 lg:hidden" />
+          <Image src="/brand/mission-wordmark-blue.png" alt="Mission Technology Solutions" width={4980} height={539} priority className="hidden h-auto w-[260px] lg:block xl:w-[300px]" />
         </Link>
         <nav className="hidden items-center gap-5 xl:gap-6 lg:flex" aria-label="Primary navigation">
           {nav.map((item) => {
@@ -89,3 +89,4 @@ export function SiteHeader() {
     </header>
   );
 }
+
